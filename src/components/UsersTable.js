b00ff25056df0e8f8f92a6e4f400e4afe79e5656
admin <maximsushkin02@gmail.com>
2022-06-29 13:59:19 +0300

    Небольшая стилизация

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,32 +1,39 @@
 import {useSelector} from "react-redux";
 import {getUsers} from "../redux/usersSlice";
-import {Table} from "antd";
+import {Button, Table} from "antd";
+import ButtonGroup from "antd/es/button/button-group";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 
 const UsersTable = () => {
   const datasource = useSelector(getUsers);
 
   const columns = [
     {
+      align: "center",
       title: "ID",
       dataIndex: "id",
       key: "id",
     },
     {
+      align: "center",
       title: "Username",
       dataIndex: "username",
       key: "username",
     },
     {
+      align: "center",
       title: "Email",
       dataIndex: "email",
       key: "email",
     },
     {
+      align: "center",
       title: "Address",
       dataIndex: "address",
       key: "address",
     },
     {
+      align: "center",
       title: "Actions",
       key: "action",
       render: () => {
