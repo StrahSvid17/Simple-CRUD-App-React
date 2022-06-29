cba929dd902ccfc688b3baab26ed729478cb89b2
admin <maximsushkin02@gmail.com>
2022-06-29 14:00:35 +0300

    Добавлен useEffect и таблица в главный компонент

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import FormCreate from "./components/FormCreate";
 import Header from "./components/Header";
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {fetchUsers} from "./redux/usersSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "./redux/usersSlice";
+import UsersTable from "./components/UsersTable";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchUsers())
-  }, [dispatch])
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   return (
     <>
       <Header />
       <FormCreate />
+      <UsersTable />
     </>
   );
 }
